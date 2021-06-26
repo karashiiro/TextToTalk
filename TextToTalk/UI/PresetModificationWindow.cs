183a9e1c51699e28fdc24ac361a480695bbc50aa
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-06-26 11:10:09 -0700

    Change some strings

--- TextToTalk/UI/PresetModificationWindow.cs
+++ TextToTalk/UI/PresetModificationWindow.cs
@@ -34,14 +34,14 @@ namespace TextToTalk.UI
 
                 ImGui.PushItemWidth(100f);
                 var kItem1 = VirtualKey.EnumToIndex(preset.ModifierKey);
-                if (ImGui.Combo("##TextToTalkKeybind1", ref kItem1, VirtualKey.Names.Take(3).ToArray(), 3))
+                if (ImGui.Combo("##PresetKeybind1", ref kItem1, VirtualKey.Names.Take(3).ToArray(), 3))
                 {
                     preset.ModifierKey = VirtualKey.IndexToEnum(kItem1);
                     Configuration.Save();
                 }
                 ImGui.SameLine();
                 var kItem2 = VirtualKey.EnumToIndex(preset.MajorKey) - 3;
-                if (ImGui.Combo("TTS Toggle Keybind##TextToTalkKeybind2", ref kItem2, VirtualKey.Names.Skip(3).ToArray(), VirtualKey.Names.Length - 3))
+                if (ImGui.Combo("Preset Enable Keybind##PresetKeybind2", ref kItem2, VirtualKey.Names.Skip(3).ToArray(), VirtualKey.Names.Length - 3))
                 {
                     preset.MajorKey = VirtualKey.IndexToEnum(kItem2) + 3;
                     Configuration.Save();
