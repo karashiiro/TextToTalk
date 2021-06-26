cf64445d9b5c049bc8c9d840b4fe7ed1ea329688
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-06-26 12:10:21 -0700

    Only show keybind setting when the keybind option is checked

--- TextToTalk/UI/PresetModificationWindow.cs
+++ TextToTalk/UI/PresetModificationWindow.cs
@@ -29,21 +29,24 @@ namespace TextToTalk.UI
                     Configuration.Save();
                 }
 
-                ImGui.PushItemWidth(100f);
-                var kItem1 = VirtualKey.EnumToIndex(preset.ModifierKey);
-                if (ImGui.Combo("##PresetKeybind1", ref kItem1, VirtualKey.Names.Take(3).ToArray(), 3))
+                if (useKeybind)
                 {
-                    preset.ModifierKey = VirtualKey.IndexToEnum(kItem1);
-                    Configuration.Save();
+                    ImGui.PushItemWidth(100f);
+                    var kItem1 = VirtualKey.EnumToIndex(preset.ModifierKey);
+                    if (ImGui.Combo("##PresetKeybind1", ref kItem1, VirtualKey.Names.Take(3).ToArray(), 3))
+                    {
+                        preset.ModifierKey = VirtualKey.IndexToEnum(kItem1);
+                        Configuration.Save();
+                    }
+                    ImGui.SameLine();
+                    var kItem2 = VirtualKey.EnumToIndex(preset.MajorKey) - 3;
+                    if (ImGui.Combo("Preset Enable Keybind##PresetKeybind2", ref kItem2, VirtualKey.Names.Skip(3).ToArray(), VirtualKey.Names.Length - 3))
+                    {
+                        preset.MajorKey = VirtualKey.IndexToEnum(kItem2 + 3);
+                        Configuration.Save();
+                    }
+                    ImGui.PopItemWidth();
                 }
-                ImGui.SameLine();
-                var kItem2 = VirtualKey.EnumToIndex(preset.MajorKey) - 3;
-                if (ImGui.Combo("Preset Enable Keybind##PresetKeybind2", ref kItem2, VirtualKey.Names.Skip(3).ToArray(), VirtualKey.Names.Length - 3))
-                {
-                    preset.MajorKey = VirtualKey.IndexToEnum(kItem2 + 3);
-                    Configuration.Save();
-                }
-                ImGui.PopItemWidth();
 
                 ImGui.Spacing();
 
