d473edb21bdb9f5d78add03b1534a254d73e1de4
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-07-01 20:51:42 -0700

    Add password entry to Polly configuration

--- TextToTalk/Backends/Polly/AmazonPollyBackend.cs
+++ TextToTalk/Backends/Polly/AmazonPollyBackend.cs
@@ -1,24 +1,42 @@
-﻿using Amazon;
+﻿using AdysTech.CredentialManager;
+using Amazon;
+using Amazon.Polly;
 using Amazon.Polly.Model;
+using ImGuiNET;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Amazon.Polly;
-using ImGuiNET;
+using System.Net;
+using System.Numerics;
 using Gender = TextToTalk.GameEnums.Gender;
 
 namespace TextToTalk.Backends.Polly
 {
     public class AmazonPollyBackend : VoiceBackend
     {
-        private readonly PollyClient polly;
-        private readonly PluginConfiguration config;
+        private const string CredentialsTarget = "TextToTalk_AccessKeys_AmazonPolly";
 
+        private static readonly Vector4 HintColor = new Vector4(0.7f, 0.7f, 0.7f, 1.0f);
+
+        private readonly PluginConfiguration config;
         private readonly IList<Voice> voices;
+
+        private PollyClient polly;
+
+        private string accessKey = string.Empty;
+        private string secretKey = string.Empty;
 
         public AmazonPollyBackend(PluginConfiguration config)
         {
-            this.polly = new PollyClient("", "", RegionEndpoint.EUWest1);
+            var credentials = CredentialManager.GetCredentials(CredentialsTarget);
+
+            if (credentials != null)
+            {
+                this.accessKey = credentials.UserName;
+                this.secretKey = credentials.Password;
+                this.polly = new PollyClient(credentials.UserName, credentials.Password, RegionEndpoint.EUWest1);
+            }
+
             this.config = config;
 
             this.voices = this.polly.GetVoicesForEngine(this.config.PollyEngine);
@@ -47,6 +65,20 @@ namespace TextToTalk.Backends.Polly
 
         public override void DrawSettings(ImExposedFunctions helpers)
         {
+            ImGui.InputTextWithHint("##TTTAccessKey", "Access key", ref this.accessKey, 100, ImGuiInputTextFlags.Password);
+            ImGui.InputTextWithHint("##TTTSecretKey", "Secret key", ref this.secretKey, 100, ImGuiInputTextFlags.Password);
+
+            if (ImGui.Button("Save##TTTSavePollyAuth"))
+            {
+                var credentials = new NetworkCredential(this.accessKey, this.secretKey);
+                CredentialManager.SaveCredentials(CredentialsTarget, credentials);
+
+                this.polly?.Dispose();
+                this.polly = new PollyClient(this.accessKey, this.secretKey, RegionEndpoint.EUWest1);
+            }
+
+            ImGui.TextColored(HintColor, "Credentials secured with Windows Credential Manager");
+
             var voiceArray = voices.Select(v => v.Name).ToArray();
             var voiceIdArray = voices.Select(v => v.Id).ToArray();
 
@@ -91,7 +123,7 @@ namespace TextToTalk.Backends.Polly
         {
             if (disposing)
             {
-                this.polly.Dispose();
+                this.polly?.Dispose();
             }
         }
     }
