71ebf1048ee14e14849334ed13b6ff65e8cc91a5
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-07-09 06:35:39 -0700

    Replace emdashes with spaced hyphens (Closes #29)

--- TextToTalk/Backends/Polly/AmazonPollyBackend.cs
+++ TextToTalk/Backends/Polly/AmazonPollyBackend.cs
@@ -67,7 +67,7 @@ namespace TextToTalk.Backends.Polly
                 .FirstOrDefault(id => id == voiceIdStr) ?? VoiceId.Matthew;
 
             PluginLog.Log(text);
-            text = text.Replace("─", "<break strength=\"strong\"/>");
+            text = text.Replace("─", " - ");
             text = $"<speak><prosody rate=\"{this.config.PollyPlaybackRate}%\">{text}</prosody></speak>";
 
             _ = this.polly.Say(this.config.PollyEngine, voiceId, this.config.PollySampleRate, this.config.PollyVolume, text);
