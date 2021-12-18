db19de501265209d6cc5402e33042a0a6b18cae9
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-12-18 12:58:00 -0800

    Add lexicon updater project

---
+++ src/TextToTalk.LexiconUpdater/LexiconPackageInfo.cs
@@ -0,0 +1,25 @@
+﻿namespace TextToTalk.LexiconUpdater
+{
+    public class LexiconPackageInfo
+    {
+        /// <summary>
+        /// The lexicon package's name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The lexicon author's username.
+        /// </summary>
+        public string Author { get; set; }
+
+        /// <summary>
+        /// The lexicon package's description.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// The files that are part of the lexicon package.
+        /// </summary>
+        public string[] Files { get; set; }
+    }
+}
