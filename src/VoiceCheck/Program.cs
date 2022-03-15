4d74128afefdad4393ea2418a27fb50e10bb3f8d
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-03-15 15:45:41 -0700

    Add system voice checker for debugging

---
+++ src/VoiceCheck/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Speech.Synthesis;
+
+namespace VoiceCheck
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            using var ss = new SpeechSynthesizer();
+            var voices = ss.GetInstalledVoices()
+                .Where(iv => iv?.Enabled ?? false)
+                .Select(v => v.VoiceInfo?.Name)
+                .ToArray();
+            var loadStatus = voices.Select(v =>
+            {
+                try
+                {
+                    ss.SelectVoice(v);
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }).ToArray();
+
+            var header = $"{"Voice",-30} | Loaded";
+            Console.WriteLine(header);
+            Console.WriteLine(new string('-', 31) + '+' + new string('-', header.Length - 31));
+            Console.Write(voices.Zip(loadStatus).Aggregate("", (agg, next) => agg + $"{next.First,-30} | {next.Second}\n"));
+        }
+    }
+}
