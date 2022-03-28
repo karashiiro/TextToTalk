bd283a955f9f13411d66f2330a121374b33fb2f5
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-03-28 12:07:04 -0700

    Refactor backends to separate out UI (again)

---
+++ src/TextToTalk/Backends/Polly/PollyCredentialManager.cs
@@ -0,0 +1,26 @@
+﻿using AdysTech.CredentialManager;
+using System.Net;
+
+namespace TextToTalk.Backends.Polly;
+
+public static class PollyCredentialManager
+{
+    private const string CredentialsTarget = "TextToTalk_AccessKeys_AmazonPolly";
+
+    public static NetworkCredential LoadCredentials()
+    {
+        var credentials = CredentialManager.GetCredentials(CredentialsTarget);
+        return credentials;
+    }
+
+    public static void SaveCredentials(string username, string password)
+    {
+        var credentials = new NetworkCredential(username, password);
+        CredentialManager.SaveCredentials(CredentialsTarget, credentials);
+    }
+
+    public static void DeleteCredentials()
+    {
+        CredentialManager.RemoveCredentials(CredentialsTarget);
+    }
+}
