8f6f662ece1d9b799f1d4aeb24355a2d9c8e8470
karashiiro <49822414+karashiiro@users.noreply.github.com>
2023-03-18 21:35:29 -0700

    Add notes on render improvements

--- src/TextToTalk/UI/ConfigComponents.cs
+++ src/TextToTalk/UI/ConfigComponents.cs
@@ -2,6 +2,10 @@
 
 namespace TextToTalk.UI;
 
+/// <summary>
+/// Generated UI components for the configuration class.
+/// Using source generation sped up the average UI render pass by a factor of 3, e.g. 0.3ms down to 0.1ms or less.
+/// </summary>
 [UseConfigComponents(typeof(PluginConfiguration))]
 public partial class ConfigComponents
 {
