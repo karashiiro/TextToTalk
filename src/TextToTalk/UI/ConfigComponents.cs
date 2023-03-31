6812fad39a736354606f29c8c0cc0e14c999a71f
karashiiro <49822414+karashiiro@users.noreply.github.com>
2023-03-31 08:10:30 -0700

    Make solution cross-platform friendly

--- src/TextToTalk/UI/ConfigComponents.cs
+++ src/TextToTalk/UI/ConfigComponents.cs
@@ -4,7 +4,9 @@ namespace TextToTalk.UI;
 
 /// <summary>
 /// Generated UI components for the configuration class.
-/// Using source generation sped up the average UI render pass by a factor of 3, e.g. 0.3ms down to 0.1ms or less.
+///
+/// Seeing unresolved symbol errors in Rider? Make sure to build
+/// TextToTalk.UI.SourceGeneration, and then restart your IDE.
 /// </summary>
 [UseConfigComponents(typeof(PluginConfiguration))]
 public partial class ConfigComponents
