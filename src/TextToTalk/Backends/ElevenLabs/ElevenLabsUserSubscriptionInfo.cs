f98ad5d8df09d3da713024eabdbf86dcc038855a
karashiiro <49822414+karashiiro@users.noreply.github.com>
2023-05-27 12:41:43 -0700

    Show quota reset time

--- src/TextToTalk/Backends/ElevenLabs/ElevenLabsUserSubscriptionInfo.cs
+++ src/TextToTalk/Backends/ElevenLabs/ElevenLabsUserSubscriptionInfo.cs
@@ -7,4 +7,7 @@ public class ElevenLabsUserSubscriptionInfo
     [JsonProperty("character_count")] public int CharacterCount { get; init; }
 
     [JsonProperty("character_limit")] public int CharacterLimit { get; init; }
+
+    [JsonProperty("next_character_count_reset_unix")]
+    public int NextCharacterCountResetUnix { get; init; }
 }
