3b2dedd69559db26ebb0c378e343822a8eb94c40
karashiiro <49822414+karashiiro@users.noreply.github.com>
2023-10-16 22:23:24 -0700

    Update Uberduck client for latest API version

--- src/TextToTalk/Backends/Uberduck/UberduckClient.cs
+++ src/TextToTalk/Backends/Uberduck/UberduckClient.cs
@@ -7,54 +7,67 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace TextToTalk.Backends.Uberduck;
 
-public class UberduckClient
+public partial class UberduckClient
 {
     private const string UrlBase = "https://api.uberduck.ai";
 
     private readonly HttpClient http;
     private readonly StreamSoundQueue soundQueue;
 
-    public string ApiKey { private get; set; }
-    public string ApiSecret { private get; set; }
+    public string? ApiKey { private get; set; }
+    public string? ApiSecret { private get; set; }
+
+    private IList<UberduckVoice> Voices { get; set; }
 
     public UberduckClient(StreamSoundQueue soundQueue, HttpClient http)
     {
         this.http = http;
         this.soundQueue = soundQueue;
+
+        Voices = new List<UberduckVoice>();
     }
 
     public async Task<IDictionary<string, IList<UberduckVoice>>> GetVoices()
     {
-        var voicesRes = await this.http.GetStringAsync(new Uri("https://api.uberduck.ai/voices?mode=tts-basic"));
-        var voices = JsonConvert.DeserializeObject<IList<UberduckVoice>>(voicesRes);
-        return voices
+        if (Voices.Count == 0) await UpdateVoices();
+        return Voices
             .GroupBy(v => v.Category)
             .ToImmutableSortedDictionary(
                 g => g.Key,
                 g => (IList<UberduckVoice>)g.OrderByDescending(v => v.DisplayName).ToList());
     }
 
-    public async Task Say(string? voice, int playbackRate, float volume, TextSource source, string text)
+    public async Task Say(string voice, int playbackRate, float volume, TextSource source, string text)
     {
         if (!IsAuthorizationSet())
         {
             throw new UberduckMissingCredentialsException("No Uberduck authorization keys have been configured.");
         }
 
+        ArgumentException.ThrowIfNullOrEmpty(voice);
+
+        var voiceModelUuid = IsUuid(voice) ? voice : await GetUuidForVoice(voice);
         var args = new UberduckSpeechRequest
         {
             Speech = text,
-            Voice = voice,
+            VoiceModelUuid = voiceModelUuid,
         };
 
         // Make the request
-        using var content = new StringContent(JsonConvert.SerializeObject(args));
+        using var content = new StringContent(JsonConvert.SerializeObject(args), Encoding.UTF8, "application/json");
         var res = await SendRequest<UberduckSpeechResponse>("/speak", reqContent: content);
-        var uuid = res.Uuid;
+        var uuid = res?.Uuid;
+        if (uuid is null)
+        {
+            DetailedLog.Warn("Got null UUID from Uberduck");
+            return;
+        }
+
         DetailedLog.Debug($"Got request UUID {uuid} from Uberduck");
 
         // Poll for the TTS result
@@ -66,7 +79,7 @@ public class UberduckClient
             try
             {
                 var status = await GetSpeechStatus(uuid);
-                if (status.FailedAt != null)
+                if (status is not { FailedAt: null })
                 {
                     DetailedLog.Warn($"TTS request {uuid} failed for an unknown reason");
                     return;
@@ -93,13 +106,34 @@ public class UberduckClient
         this.soundQueue.EnqueueSound(waveStream, source, StreamFormat.Wave, volume);
     }
 
-    private Task<UberduckSpeechStatusResponse> GetSpeechStatus(string uuid)
+    private Task<UberduckSpeechStatusResponse?> GetSpeechStatus(string uuid)
     {
         return SendRequest<UberduckSpeechStatusResponse>("/speak-status", $"uuid={uuid}");
     }
 
-    private async Task<TResponse> SendRequest<TResponse>(string endpoint, string query = "",
-        HttpContent reqContent = null) where TResponse : class
+    [GeneratedRegex("[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}", RegexOptions.IgnoreCase)]
+    private static partial Regex UuidRegex();
+
+    private static bool IsUuid(string voice)
+    {
+        return UuidRegex().IsMatch(voice);
+    }
+
+    private async Task<string> GetUuidForVoice(string voice)
+    {
+        if (Voices.Count == 0) await UpdateVoices();
+        var voiceInfo = Voices.Single(v => v.Name == voice);
+        return voiceInfo.VoiceModelUuid;
+    }
+
+    private async Task UpdateVoices()
+    {
+        var voicesRes = await this.http.GetStringAsync(new Uri("https://api.uberduck.ai/voices?mode=tts-basic"));
+        Voices = JsonConvert.DeserializeObject<IList<UberduckVoice>>(voicesRes) ?? new List<UberduckVoice>();
+    }
+
+    private async Task<TResponse?> SendRequest<TResponse>(string endpoint, string query = "",
+        HttpContent? reqContent = null) where TResponse : class
     {
         var uriBuilder = new UriBuilder(UrlBase)
         {
@@ -119,17 +153,32 @@ public class UberduckClient
         var resContent = await res.Content.ReadAsStringAsync();
         if (res.StatusCode is HttpStatusCode.Unauthorized or HttpStatusCode.Forbidden)
         {
-            var detail = JsonConvert.DeserializeObject<UberduckFailedResponse>(resContent)?.Detail;
+            var detail = GetRequestFailureDetail(resContent);
             throw new UberduckUnauthorizedException(detail);
         }
 
         if (!res.IsSuccessStatusCode)
         {
-            var detail = JsonConvert.DeserializeObject<UberduckFailedResponse>(resContent)?.Detail;
+            var detail = GetRequestFailureDetail(resContent);
             throw new UberduckFailedException(res.StatusCode, detail);
         }
 
         return JsonConvert.DeserializeObject<TResponse>(resContent);
+    }
+
+    private static string GetRequestFailureDetail(string resContent)
+    {
+        try
+        {
+            var detail = JsonConvert.DeserializeObject<UberduckFailedResponse>(resContent)?.Detail ??
+                         new List<UberduckFailedResponseEntry>();
+            return string.Join("\n", detail.Select(e => $"({e.Type}:{string.Join(",", e.Location)}) {e.Message}"));
+        }
+        catch (JsonReaderException ex)
+        {
+            throw new AggregateException(
+                $"Could not parse {nameof(UberduckFailedResponse)} from string \"{resContent}\"", ex);
+        }
     }
 
     private void AddAuthorization(HttpRequestMessage req)
@@ -142,24 +191,33 @@ public class UberduckClient
 
     private bool IsAuthorizationSet()
     {
-        return ApiKey.Length > 0 && ApiSecret.Length > 0;
+        return ApiKey?.Length > 0 && ApiSecret?.Length > 0;
     }
 
     private class UberduckSpeechRequest
     {
-        [JsonProperty("speech")] public string Speech { get; init; }
+        [JsonProperty("speech")] public required string Speech { get; init; }
 
-        [JsonProperty("voice")] public string? Voice { get; init; }
+        [JsonProperty("voicemodel_uuid")] public string? VoiceModelUuid { get; init; }
     }
 
     private class UberduckSpeechResponse
     {
-        [JsonProperty("uuid")] public string Uuid { get; init; }
+        [JsonProperty("uuid")] public required string Uuid { get; init; }
     }
 
     private class UberduckFailedResponse
     {
-        [JsonProperty("detail")] public string Detail { get; set; }
+        [JsonProperty("detail")] public required IList<UberduckFailedResponseEntry> Detail { get; set; }
+    }
+
+    private class UberduckFailedResponseEntry
+    {
+        [JsonProperty("loc")] public required IList<string> Location { get; set; }
+
+        [JsonProperty("msg")] public required string Message { get; set; }
+
+        [JsonProperty("type")] public required string Type { get; set; }
     }
 
     private class UberduckSpeechStatusResponse
@@ -170,6 +228,6 @@ public class UberduckClient
 
         [JsonProperty("finished_at")] public DateTime? FinishedAt { get; init; }
 
-        [JsonProperty("path")] public string Path { get; init; }
+        [JsonProperty("path")] public required string Path { get; init; }
     }
 }
