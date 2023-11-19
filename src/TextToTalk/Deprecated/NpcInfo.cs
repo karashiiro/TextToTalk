11f0d729a33ad97091ace65ef7a0f7efc58283f6
karashiiro <49822414+karashiiro@users.noreply.github.com>
2023-11-19 14:54:58 -0800

    Add comments for deprecated classes

--- src/TextToTalk/Deprecated/NpcInfo.cs
+++ src/TextToTalk/Deprecated/NpcInfo.cs
@@ -2,6 +2,10 @@
 // ReSharper disable CheckNamespace
 namespace TextToTalk;
 
+/// <summary>
+/// This class used to represent <see cref="TextToTalk.Data.Model.Npc"/>, but now exists only to avoid
+/// breaking config upgrades from pre-1.25.0.
+/// </summary>
 public class NpcInfo
 {
     public string? Name { get; }
--- src/TextToTalk/Deprecated/PlayerInfo.cs
+++ src/TextToTalk/Deprecated/PlayerInfo.cs
@@ -2,6 +2,10 @@
 // ReSharper disable CheckNamespace
 namespace TextToTalk;
 
+/// <summary>
+/// This class used to represent <see cref="TextToTalk.Data.Model.Player"/>, but now exists only to avoid
+/// breaking config upgrades from pre-1.25.0.
+/// </summary>
 public class PlayerInfo
 {
     public string? Name { get; }
