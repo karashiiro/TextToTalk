7d12140451aabd6d787ea793a2185a319e797bca
karashiiro <49822414+karashiiro@users.noreply.github.com>
2023-11-19 09:39:14 -0800

    Cast deprecated type to dynamic and then JObject

--- src/TextToTalk/Migrations/Migration1_25_0.cs
+++ src/TextToTalk/Migrations/Migration1_25_0.cs
@@ -46,7 +46,7 @@ public class Migration1_25_0 : IConfigurationMigration
             }
             catch (RuntimeBinderException)
             {
-                if (!playerInfo.TryGetValue("Name", out JToken name))
+                if (!((playerInfo as dynamic as JObject)!).TryGetValue("Name", out var name))
                 {
                     continue;
                 }
@@ -84,7 +84,7 @@ public class Migration1_25_0 : IConfigurationMigration
             {
                 // This degraded to a JObject since the original type was deleted and
                 // the new field type is dynamic.
-                if (!npcInfo.TryGetValue("Name", out JToken name))
+                if (!(npcInfo as dynamic as JObject)!.TryGetValue("Name", out var name))
                 {
                     continue;
                 }
