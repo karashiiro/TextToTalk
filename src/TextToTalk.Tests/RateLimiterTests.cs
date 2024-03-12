9682abe8aae8429ad13b5ea820d3f1b962d24230
karashiiro <49822414+karashiiro@users.noreply.github.com>
2024-03-11 22:54:58 -0700

    Convert RateLimiterTests to file-scoped namespace

--- src/TextToTalk.Tests/RateLimiterTests.cs
+++ src/TextToTalk.Tests/RateLimiterTests.cs
@@ -2,48 +2,47 @@ using System.Threading.Tasks;
 using TextToTalk.Middleware;
 using Xunit;
 
-namespace TextToTalk.Tests
+namespace TextToTalk.Tests;
+
+public class RateLimiterTests
 {
-    public class RateLimiterTests
+    [Fact]
+    public async Task RateLimiter_Works_WhenNotLimited()
     {
-        [Fact]
-        public async Task RateLimiter_Works_WhenNotLimited()
-        {
-            const string speaker = "12345";
-            using var limiter = new RateLimiter(() => 200);
-            Assert.False(limiter.TryRateLimit(speaker));
-            await Task.Delay(500);
-            Assert.False(limiter.TryRateLimit(speaker));
-            await Task.Delay(500);
-            Assert.False(limiter.TryRateLimit(speaker));
-            await Task.Delay(500);
-            Assert.False(limiter.TryRateLimit(speaker));
-        }
+        const string speaker = "12345";
+        using var limiter = new RateLimiter(() => 200);
+        Assert.False(limiter.TryRateLimit(speaker));
+        await Task.Delay(500);
+        Assert.False(limiter.TryRateLimit(speaker));
+        await Task.Delay(500);
+        Assert.False(limiter.TryRateLimit(speaker));
+        await Task.Delay(500);
+        Assert.False(limiter.TryRateLimit(speaker));
+    }
 
-        [Fact]
-        public async Task RateLimiter_Works_WhenLimited()
-        {
-            const string speaker = "12345";
-            using var limiter = new RateLimiter(() => 200);
-            Assert.False(limiter.TryRateLimit(speaker));
-            await Task.Delay(50);
-            Assert.True(limiter.TryRateLimit(speaker));
-            await Task.Delay(50);
-            Assert.True(limiter.TryRateLimit(speaker));
-            await Task.Delay(50);
-            Assert.True(limiter.TryRateLimit(speaker));
-        }
+    [Fact]
+    public async Task RateLimiter_Works_WhenLimited()
+    {
+        const string speaker = "12345";
+        using var limiter = new RateLimiter(() => 200);
+        Assert.False(limiter.TryRateLimit(speaker));
+        await Task.Delay(50);
+        Assert.True(limiter.TryRateLimit(speaker));
+        await Task.Delay(50);
+        Assert.True(limiter.TryRateLimit(speaker));
+        await Task.Delay(50);
+        Assert.True(limiter.TryRateLimit(speaker));
+    }
 
-        [Fact]
-        public async Task RateLimiter_Works_Mixed()
-        {
-            const string speaker = "12345";
-            using var limiter = new RateLimiter(() => 200);
-            Assert.False(limiter.TryRateLimit(speaker));
-            await Task.Delay(500);
-            Assert.False(limiter.TryRateLimit(speaker));
-            await Task.Delay(50);
-            Assert.True(limiter.TryRateLimit(speaker));
-        }
+    [Fact]
+    public async Task RateLimiter_Works_Mixed()
+    {
+        const string speaker = "12345";
+        using var limiter = new RateLimiter(() => 200);
+        Assert.False(limiter.TryRateLimit(speaker));
+        await Task.Delay(500);
+        Assert.False(limiter.TryRateLimit(speaker));
+        await Task.Delay(50);
+        Assert.True(limiter.TryRateLimit(speaker));
     }
 }
