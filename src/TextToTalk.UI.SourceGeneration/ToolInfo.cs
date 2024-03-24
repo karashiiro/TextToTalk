fa3a1e8e2cba2b86d698a8174bfe33f4cfb777c3
karashiiro <49822414+karashiiro@users.noreply.github.com>
2024-03-23 23:50:09 -0700

    Catch and log ToolInfo errors

--- src/TextToTalk.UI.SourceGeneration/ToolInfo.cs
+++ src/TextToTalk.UI.SourceGeneration/ToolInfo.cs
@@ -1,13 +1,45 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.Reflection;
 
 namespace TextToTalk.UI.SourceGeneration;
 
 internal static class ToolInfo
 {
-    internal static readonly Assembly ToolAssembly = typeof(ConfigComponentsGenerator).Assembly;
+    internal static readonly Assembly ToolAssembly =
+        RunWithConsoleLogging(() => typeof(ConfigComponentsGenerator).Assembly);
 
-    internal static readonly string AssemblyName = ToolAssembly.GetName().Name!;
+    internal static readonly string AssemblyName =
+        RunWithConsoleLogging(() => ToolAssembly.GetName().Name!);
 
-    internal static readonly string AssemblyVersion = FileVersionInfo.GetVersionInfo(ToolAssembly.Location).FileVersion!;
+    internal static readonly string AssemblyVersion =
+        RunWithConsoleLogging(() => FileVersionInfo.GetVersionInfo(ToolAssembly.Location).FileVersion!);
+
+    private static T RunWithConsoleLogging<T>(Func<T> fn)
+    {
+        try
+        {
+            return fn();
+        }
+        catch (Exception ex)
+        {
+            LogException(ex);
+            throw;
+        }
+    }
+
+    private static void LogException(Exception ex)
+    {
+        while (true)
+        {
+            Console.WriteLine(ex);
+            if (ex.InnerException != null)
+            {
+                ex = ex.InnerException;
+                continue;
+            }
+
+            break;
+        }
+    }
 }
