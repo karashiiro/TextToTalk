358060a0060dfab2afe581032cb58a0df22aa663
karashiiro <49822414+karashiiro@users.noreply.github.com>
2024-03-24 00:10:53 -0700

    Support a top-level VoiceUnlocker because Plogon is weird

--- src/TextToTalk/VoiceUnlockerRunner.cs
+++ src/TextToTalk/VoiceUnlockerRunner.cs
@@ -17,9 +17,7 @@ namespace TextToTalk
         {
             try
             {
-                var assemblyPath = Path.Combine(this.assemblyLocation, "..");
-                var applicationPath = Path.Combine(assemblyPath, "VoiceUnlocker", "VoiceUnlocker.exe");
-                using var application = Process.Start(new ProcessStartInfo(applicationPath)
+                using var application = Process.Start(new ProcessStartInfo(GetVoiceUnlockerPath())
                 {
                     UseShellExecute = true,
                     Verb = "runas",
@@ -40,6 +38,19 @@ namespace TextToTalk
             }
 
             return true;
+        }
+
+        private string GetVoiceUnlockerPath()
+        {
+            var assemblyPath = Path.Combine(this.assemblyLocation, "..");
+
+            var applicationPath1 = Path.Combine(assemblyPath, "VoiceUnlocker", "VoiceUnlocker.exe");
+            if (File.Exists(applicationPath1)) return applicationPath1;
+
+            var applicationPath2 = Path.Combine(assemblyPath, "VoiceUnlocker.exe");
+            if (File.Exists(applicationPath2)) return applicationPath2;
+
+            throw new FileNotFoundException("The VoiceUnlocker executable could not be found.", applicationPath1);
         }
     }
 }
