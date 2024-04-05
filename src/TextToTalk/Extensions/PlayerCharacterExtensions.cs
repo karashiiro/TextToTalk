623b06c7889659579e0d43f4e1727395f3ddcd8b
karashiiro <49822414+karashiiro@users.noreply.github.com>
2024-04-05 07:15:05 -0700

    Make first/last name code more concise

--- src/TextToTalk/Extensions/PlayerCharacterExtensions.cs
+++ src/TextToTalk/Extensions/PlayerCharacterExtensions.cs
@@ -7,13 +7,13 @@ public static class PlayerCharacterExtensions
     public static string? GetFirstName(this PlayerCharacter playerCharacter)
     {
         var parts = playerCharacter.GetFullName().Split(" ");
-        return parts is [{ } firstName, not null] ? firstName : null;
+        return parts is [{ } firstName, _] ? firstName : null;
     }
 
     public static string? GetLastName(this PlayerCharacter playerCharacter)
     {
         var parts = playerCharacter.GetFullName().Split(" ");
-        return parts is [not null, { } lastName] ? lastName : null;
+        return parts is [_, { } lastName] ? lastName : null;
     }
 
     public static string GetFullName(this PlayerCharacter playerCharacter)
