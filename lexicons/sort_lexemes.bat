892476300a64416c2184daf3f5000641ca07c33e
Ryan <3673962+ryankhart@users.noreply.github.com>
2024-07-14 13:43:51 -0700

    Make sort .bat file sort in place by default

--- lexicons/sort_lexemes.bat
+++ lexicons/sort_lexemes.bat
@@ -3,7 +3,7 @@ setlocal
 
 REM Input and output file names
 set input_file=lexicon.pls
-set output_file=sorted_lexicon.pls
+set output_file=lexicon.pls
 
 REM If %1 is set, then set the input file to %1
 if not "%1" == "" set input_file=%1
