0a073ebb21bff7356d0dfe4c69588c26af2c6d58
PhilippNaused <philipp.naused@t-online.de>
2025-06-07 00:47:30 +0200

    add source-generated tooltips for config components

---
+++ src/TextToTalk.UI.Core/TooltipAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace TextToTalk.UI.Core;
+
+[AttributeUsage(AttributeTargets.Property)]
+public class TooltipAttribute : Attribute
+{
+    public string Text { get; }
+    public TooltipAttribute(string text) => Text = text;
+}
