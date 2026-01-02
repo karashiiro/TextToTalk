eeab65979675c79402296bf4b03d3f7af6d05eff
mitcheb0219 <mitcheb0219@gmail.com>
2026-01-02 13:06:31 -0500

    Update SystemSoundQueue.cs

--- src/TextToTalk/Backends/System/SystemSoundQueue.cs
+++ src/TextToTalk/Backends/System/SystemSoundQueue.cs
@@ -76,14 +76,29 @@ namespace TextToTalk.Backends.System
                 1f); // Hard coded 1f for volume float as ssml already takes care of user volume input
         }
 
+        public override void CancelAllSounds()
+        {
+            base.CancelAllSounds();
+            this.streamSoundQueue.CancelAllSounds();
+        }
+
+        public override void CancelFromSource(TextSource source)
+        {
+            base.CancelFromSource(source);
+            this.streamSoundQueue.CancelFromSource(source);
+        }
+
+
         protected override void OnSoundCancelled()
         {
-            try
-            {
-                this.speechSynthesizer.SpeakAsyncCancelAll();
+            try 
+            { 
+                this.speechSynthesizer.SetOutputToNull(); 
             }
+
             catch (ObjectDisposedException)
             {
+
             }
         }
 
